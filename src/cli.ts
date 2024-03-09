c6906128e2dc5ed8c9763e4d98b3c394728c4f2c
Li <dementia.aswad@gmail.com>
2024-03-09 09:58:45 -0500

    example config file

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,42 @@
 import { program } from "commander";
 import { version } from "../package.json";
 import { Reader } from "./toml";
+import { userInfo } from "node:os";
+import path from "node:path";
+
+const configUrlPath = path.resolve(
+  userInfo().homedir,
+  ".config",
+  "ddnsu",
+  "config.toml"
+);
+console.log(configUrlPath);
 
 namespace CliLookupFunction {
+  export function config_version() {
+    let version = Reader.get("version") as unknown as string;
+    console.log(version);
+    process.exit(0);
+  }
+
   export function config() {
-    console.log(Reader.get("version") as unknown as string);
+    console.log("config function called");
+  }
+
+  export function start() {
+    console.log("start function called");
+  }
+
+  export function kill() {
+    console.log("kill function called");
+  }
+
+  export function enable() {
+    console.log("enable function called");
+  }
+
+  export function install() {
+    console.log("install function called");
   }
 }
 
@@ -15,27 +47,37 @@ program.version(
 );
 program.description("Dynamic Domain Name Server Updater");
 
-program.option(
-  "-cv, --config-version",
-  "return the current version of the configuration file"
-);
+program
+  .option(
+    "-cv, --config-version",
+    "return the current version of the configuration file"
+  )
+  .action(CliLookupFunction.config_version);
 
 program
-  .command("config", "print out content of configuration file and location")
+  .command("config")
+  .description("print out content of configuration file and location")
   .action(CliLookupFunction.config);
 
-program.command("start", "start ddnsu service");
-program.command("kill", "kill all ddnsu service instances in the background");
-program.command("enable", "spawn a ddnsu service in the background");
+program
+  .command("start")
+  .description("start ddnsu service")
+  .action(CliLookupFunction.start);
+program
+  .command("kill")
+  .description("kill all ddnsu service instances in the background")
+  .action(CliLookupFunction.kill);
+program
+  .command("enable")
+  .description("spawn a ddnsu service in the background")
+  .action(CliLookupFunction.enable);
 // program.command("logs", "return logs of ddnsu");
-program.command(
-  "install",
-  "install ddnsu as a systemd service (requires sudo)"
-);
+program
+  .command("install")
+  .description("install ddnsu as a systemd service (requires sudo)")
+  .action(CliLookupFunction.install);
 
-program.parse();
+program.parse(Bun.argv);
 const options = program.opts();
-
-options["cv"];
 
 console.log(options);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import "./service";
+// import "./service";
 import "./cli";
